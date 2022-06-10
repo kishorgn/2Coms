ea1f460f128d0c06bdb8604d166c1e690abe2c51
Kishor <kishorgn@gmail.com>
2022-06-10 18:46:09 +0530

    Commit - 2 : Added for loop in prog1 and created new prog2.

--- DemoProject_01/src/com/ignite/app/App_01.java
+++ DemoProject_01/src/com/ignite/app/App_01.java
@@ -5,6 +5,11 @@ public class App_01 {
 	public static void main(String[] args) {
 		System.out.println("Hello world!!!");
 		System.out.println("Welcome to Java Programming");
+		for(int c = 0; c < 10 ; c++) {
+			System.out.println(c);
+		}
+		
+		System.out.println("Thank you!!!");
 
 	}
 
---
+++ DemoProject_01/src/com/ignite/app/App_02.java
@@ -0,0 +1,11 @@
+package com.ignite.app;
+
+public class App_02 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to 2nd program");
+
+	}
+
+}
